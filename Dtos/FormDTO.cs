a966b8a52a29a75cb140f22773490a69fda32cc2
AlaaBoukchina <alaa.boukchina@gmail.com>
2024-12-08 22:43:24 +0100

    optional items

--- Dtos/FormDTO.cs
+++ Dtos/FormDTO.cs
@@ -10,21 +10,21 @@
 
     public class FormFieldDto
     {
-        public string Id { get; set; }
-        public string FieldType { get; set; }
-        public string Label { get; set; }
-        public string Value { get; set; }
-        public bool IsRequired { get; set; }
-        public string Placeholder { get; set; }
-        public int Order { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string FieldType { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public bool IsRequired { get; set; } = false;
+        public string Placeholder { get; set; } = string.Empty;
+        public int Order { get; set; } = 0;
         public List<FormFieldOptionDto> Options { get; set; } = new List<FormFieldOptionDto>(); // Added support for options
     }
 
     public class FormFieldOptionDto
     {
-        public string Label { get; set; }
-        public string Value { get; set; }
-        public string Name { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
     }
 
 
